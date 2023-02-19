daf2be3499462738a7d1732180f3eba539c16416
Kumar Utsav <m1089430@mindtree.com>
2023-02-19 19:27:11 +0530

    Module 3 code.

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), {
 const users = JSON.parse(userData);
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -49,7 +50,31 @@ app.get('/profile', (req, res) => {
     res.render('profile', {
         user: users[0]
     })
-})
+});
+
+app.get('/transfer', (req, res) => {
+    res.render('transfer');
+});
+
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance -= parseInt(req.body.amount);
+    accounts[req.body.to].balance += parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', (req, res) => {
+    res.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= parseInt(req.body.amount);
+    accounts.credit.available += parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', { message: "Payment Successful", account: accounts.credit });
+});
 
 app.listen(3000, () => {
     console.log("PS Project Running on port 3000!");
