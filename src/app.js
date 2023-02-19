7f02462dbef3f884afbf1bfd1f783357532313b2
Kumar Utsav <m1089430@mindtree.com>
2023-02-19 07:03:21 +0530

    Module 2 code.

--- src/app.js
+++ src/app.js
@@ -4,15 +4,52 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), {
+    encoding: "UTF8"
+});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), {
+    encoding: "UTF8"
+});
+
+const users = JSON.parse(userData);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Index'
+        title: 'Account Summary',
+        accounts: accounts
     })
 });
+
+app.get('/savings', (req, res) => {
+    res.render('account', {
+        account: accounts.savings
+    })
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', {
+        account: accounts.checking
+    })
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', {
+        account: accounts.credit
+    })
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {
+        user: users[0]
+    })
+})
 
 app.listen(3000, () => {
     console.log("PS Project Running on port 3000!");
